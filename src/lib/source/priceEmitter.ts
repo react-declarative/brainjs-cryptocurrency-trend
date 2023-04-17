6323d3969946f2094b3a2d164cec77cd97ff8754
Petr Tripolsky <tripolskypetr@gmail.com>
2023-04-18 02:41:46 +0500

    price-emitter

---
+++ src/lib/source/priceEmitter.ts
@@ -0,0 +1,61 @@
+import { Source, compose, fetchApi } from 'react-declarative';
+
+export const priceEmitter = Source.multicast(() => Source.create<number>((next) => {
+
+    let disposeRef: Function = () => undefined;
+
+    const process = async () => {
+
+        let intervalRef: Function = () => undefined;
+
+        const {
+            data: {
+                instanceServers = [],
+                token = ""
+            },
+        } = await fetchApi("https://api.kucoin.com/api/v1/bullet-public", {
+            method: "POST",
+        });
+
+        const server = instanceServers.find((server: any) => server.protocol = "websocket");
+        const wsurl = new URL("endpoint", server.endpoint);
+        wsurl.searchParams.set("token", token);
+
+        const ws = new WebSocket(wsurl);
+
+        ws.addEventListener('open', () => {
+            ws.send(JSON.stringify({
+                "id": Date.now(),
+                "type": "subscribe",
+                "topic": "/market/ticker:ETH-USDT",
+                "privateChannel": false,
+                "response": true,
+            }));
+            intervalRef = Source.fromInterval(1_000).connect(() => {
+                ws.send(JSON.stringify({
+                    "id": Date.now(),
+                    "type": "ping",
+                }));
+            });
+        });
+
+        ws.addEventListener('message', (msg) => {
+            const data = JSON.parse(msg.data);
+            if (data.type === "message" && data.topic === "/market/ticker:ETH-USDT") {
+                next(data.data.price);
+            }
+        });
+
+        disposeRef = compose(
+            () => ws.close(),
+            () => intervalRef(),
+        );
+    };
+
+    process();
+
+    return () => disposeRef();
+
+}));
+
+export default priceEmitter;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { FieldType, IField } from "react-declarative";
+
 import { makeStyles } from "../../styles/makeStyles";
 
 import Paper from "@mui/material/Paper";
@@ -22,6 +24,70 @@ const useStyles = makeStyles()((theme) => ({
     padding: 15,
   },
 }));
+
+const GROW_DESCRIPTION =
+  "Tomorrow the price of your coin will start to rise. It's time to get few coins";
+
+const FAIL_DESCRIPTION =
+  "Tomorrow the price of your coin will start to fall. It's time to dump it";
+
+const createDimension = (
+  key: string,
+  title: string,
+  desc: string,
+  Icon: React.ReactElement
+): IField => ({
+  type: FieldType.Paper,
+  fieldRightMargin: "0",
+  fieldBottomMargin: "1",
+  //isVisible: (data) => !!data[key],
+  style: {
+    overflow: "hidden",
+  },
+  fields: [
+    {
+      type: FieldType.Typography,
+      typoVariant: "body1",
+      placeholder: title.toUpperCase(),
+      fieldRightMargin: "0",
+      fieldBottomMargin: "0",
+    },
+    {
+      type: FieldType.Typography,
+      fieldRightMargin: "0",
+      fieldBottomMargin: "0",
+      typoVariant: "body2",
+      placeholder: desc,
+      style: {
+        minWidth: "260px",
+      },
+    },
+    {
+      type: FieldType.Typography,
+      fieldRightMargin: "0",
+      fieldBottomMargin: "0",
+      typoVariant: "subtitle2",
+      style: {
+        color: "gray",
+      },
+      placeholder: "Consensus threshold for neural network",
+    },
+    {
+      type: FieldType.Component,
+      fieldRightMargin: "0",
+      fieldBottomMargin: "0",
+      element: () => (
+        <Box display="flex" justifyContent="center">
+          {Icon}
+        </Box>
+      ),
+    },
+    {
+      type: FieldType.Progress,
+      name: key,
+    },
+  ],
+});
 
 export const MainPage = () => {
   const { classes } = useStyles();
