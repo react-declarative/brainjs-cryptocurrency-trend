be20e59361b40f9bc52c6739fc04239402eccda7
Petr Tripolsky <tripolskypetr@gmail.com>
2023-04-18 20:54:00 +0500

    slope-helper

---
+++ src/utils/calculateSlope.ts
@@ -0,0 +1,39 @@
+/**
+ * @see https://stackoverflow.com/questions/6195335/linear-regression-in-javascript
+ * @description if Slope variable is positive the chart is going upward
+ * @description if Slope variable is negative the chart is going downward
+ */
+export const calculateSlope = (data: number[]) => {
+
+    const X: number[] = [];
+    const Y: number[] = [];
+    
+    data.forEach((item, idx) => {
+        X.push(idx);
+        Y.push(item);
+    });
+
+    let Slope = 0;
+    // let Intercept = 0;
+    let SX = 0;
+    let SY = 0;
+    let SXX = 0; 
+    let SXY = 0;
+    let SYY = 0;
+    let N = X.length;
+    
+    for (let i = 0; i < N; i++) {
+        SX = SX + X[i];
+        SY = SY + Y[i];
+        SXY = SXY + X[i] * Y[i];
+        SXX = SXX + X[i] * X[i];
+        SYY = SYY + Y[i] * Y[i];
+    }
+    
+    Slope = ((N * SXY) - (SX * SY)) / ((N * SXX) - (SX * SX));
+    // Intercept = (SY - (Slope * SX)) / N;
+
+    return Number.isFinite(Slope) ? Math.sign(Slope) : 0;
+};
+
+export default calculateSlope;
