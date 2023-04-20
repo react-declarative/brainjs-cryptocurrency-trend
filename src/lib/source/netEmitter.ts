899ba0cb47a7212d8279617317fe986a3c716b4e
Petr Tripolsky <tripolskypetr@gmail.com>
2023-04-20 23:25:50 +0500

    logs

--- src/lib/source/netEmitter.ts
+++ src/lib/source/netEmitter.ts
@@ -53,6 +53,7 @@ export const netEmitter = Source
         race: true,
     })
     .operator<[number[][], number[][]]>(Operator.take(1))
+    .tap(() => console.log(`starting trainment ${getTimeLabel(new Date())}`))
     .mapAsync(async ([positiveSet, negativeSet]) => {
         const net = new NeuralNetworkGPU({
             ...netManager.getValue()!,
