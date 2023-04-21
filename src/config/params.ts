5bbf33c3fc3c18a2f5e0f09ca9011c1a580ae35a
Petr Tripolsky <tripolskypetr@gmail.com>
2023-04-21 19:44:59 +0500

    config

--- src/config/params.ts
+++ src/config/params.ts
@@ -2,7 +2,7 @@ export const CC_INPUT_SIZE = 10;
 export const CC_OUTPUT_SIZE = 2;
 export const CC_STRIDE_STEP = 1;
 export const CC_TRAIN_WINDOW_SIZE = 100;
-export const CC_TRAIN_TARGET_SIZE = 50;
+export const CC_TRAIN_TARGET_SIZE = 45;
 export const CC_PRICE_SLOPE_ADJUST = 1_000;
 
 export const CC_NET_EMIT_THRESHOLD = 0.7;
