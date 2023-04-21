51eeb3c658e0d53791ec59afc3d62a7825871af8
Petr Tripolsky <tripolskypetr@gmail.com>
2023-04-21 17:56:27 +0500

    cleanup

--- src/config/params.ts
+++ src/config/params.ts
@@ -4,4 +4,4 @@ export const CC_TRAIN_WINDOW_SIZE = 100;
 export const CC_TRAIN_TARGET_SIZE = 70;
 export const CC_PRICE_SLOPE_ADJUST = 1_000;
 
-export const CC_NET_EMIT_THRESHOLD = 0.25;
+export const CC_NET_EMIT_THRESHOLD = 0.7;
