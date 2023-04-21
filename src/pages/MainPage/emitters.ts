e115753bf720f46e1056b8e012f1e9e273fda5ca
Petr Tripolsky <tripolskypetr@gmail.com>
2023-04-22 00:46:55 +0500

    integration

--- src/pages/MainPage/emitters.ts
+++ src/pages/MainPage/emitters.ts
@@ -5,15 +5,19 @@ export const predictEmitter = new Subject<"train" | "upward" | "downward" | "unt
 
 /**
  * TODO: implement
- *  - buys crypto with limit order (price CANDLE_HIGH_PRICE + 0.000001) and makes sell limit order (price AVERAGE_BUY_PRICE + 1%)
- *  - does nothing if see opened orders
- *  - avoid duplicated requests
+ *  - handler which buys crypto with limit order (price CANDLE_HIGH_PRICE + 0.000001) and makes sell limit order (price AVERAGE_BUY_PRICE + 1%)
+ *      1. does nothing if have pending request (avoid duplicated requests)
+ *      2. does nothing if have unresolved order (opened LIMIT_SELL or opened LIMIT_BUY with incoming LIMIT_SELL)
  */
 const doTrade = async (sellPercent: number, usdtAmount: number) => {
     try {
         await fetchApi(new URL('/api/v1/do_trade', window.location.origin), {
             method: "POST",
-            body: JSON.stringify({ symbol: "ETHUSDT", sellPercent, usdtAmount }, null, 2),
+            body: JSON.stringify({
+                symbol: "ETHUSDT",
+                sellPercent: sellPercent.toFixed(3),
+                usdtAmount: usdtAmount.toFixed(0),
+            }, null, 2),
         });
         console.log(`trade request sended ${getTimeLabel(new Date())}`);
     } catch {
