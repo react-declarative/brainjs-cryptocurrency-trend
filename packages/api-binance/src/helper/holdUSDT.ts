53ed1b31024f000f53d7e43b93fbfa5e488c0cf7
Petr Tripolsky <tripolskypetr@gmail.com>
2023-05-02 03:20:58 +0500

    import-patch

--- packages/api-binance/src/helper/holdUSDT.ts
+++ packages/api-binance/src/helper/holdUSDT.ts
@@ -1,5 +1,5 @@
 import { LoggerService } from '@nestjs/common';
-import dayjs from 'dayjs';
+import * as dayjs from 'dayjs';
 import { Binance, OrderType, SymbolFilterType } from 'binance-api-node';
 
 export const createHoldUSDT = (binance: Binance, logger: LoggerService) => {
